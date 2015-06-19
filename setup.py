df92957a290835fafe82b94f8c47382836cec95f
Andrew Click-Horn <andrewphorn@gmail.com>
2015-06-18 17:04:06 -0700

    Fix setup.py and __init__.py to allow proper installation

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='SimpleWebSocketServer',
+    version='1.1',
+    author='dpallot',
+    author_email='',
+    description='Simple python implementation of a websocket server.',
+    url='https://github.com/dpallot/simple-websocket-server/',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python'
+    ]
+)
